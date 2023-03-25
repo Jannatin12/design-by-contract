7d3e60fe1d0bced87418991920a8be347933aa92
Hasnannn <jannatinnurrohmah@gmail.com>
2023-03-25 15:47:32 +0700

    Add class SayaTubeVideo

--- tpmodul6_1302213051/Program.cs
+++ tpmodul6_1302213051/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using tpmodul6_1302213051;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nama = "Jannatin";
+        SayaTubeVideo tubeVideo = new SayaTubeVideo($"Tutorial Design By Contract - {nama}");
+
+        tubeVideo.IncreasePlayCount(5000);
+        tubeVideo.PrintVideoDetails();
+    }
+}
